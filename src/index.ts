5039b169a48d2363d70a459afbb61d319d28e0fe
Paolo Ragone <pragone@gmail.com>
2017-06-07 22:02:12 +1000

    Flat exports

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,6 @@
-import { ExtendedError } from './util/ErrorUtil';
-import * as _ErrorUtil from './util/ErrorUtil';
-import { InceptumApp } from './app/InceptumApp';
-import * as _Context from './ioc/Context';
-import * as _PreinstantiatedSingletonDefinition from './ioc/objectdefinition/PreinstantiatedSingletonDefinition';
-import * as _InceptumApp from './app/InceptumApp';
-import * as _LogManager from './log/LogManager';
+export { Context } from './ioc/Context';
+export { PreinstantiatedSingletonDefinition } from './ioc/objectdefinition/PreinstantiatedSingletonDefinition';
 
-export namespace ioc {
-  export const Context = _Context.Context;
-  export namespace objectdefinition {
-    export const PreinstantiatedSingletonDefinition = _PreinstantiatedSingletonDefinition.PreinstantiatedSingletonDefinition;
-  }
-}
-
-export namespace app {
-  export const InceptumApp = _InceptumApp.InceptumApp;
-}
-
-export namespace log {
-  export const LogManager = _LogManager.LogManager;
-  export const Logger = _LogManager.Logger;
-}
-
-export namespace util {
-  export const ExtendedError = _ErrorUtil.ExtendedError;
-}
+export { InceptumApp } from './app/InceptumApp';
+export * from './log/LogManager';
+export { ExtendedError } from './util/ErrorUtil';
