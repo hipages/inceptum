03b9f84a8af09b9438e0a69addcf67425af9b09d
Siyang Bi <siyang.bi@gmail.com>
2017-09-04 14:01:44 +1000

    *Fix lint

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-import "./util/BluePatch";
+import './util/BluePatch';
 
-export { Context } from "./ioc/Context";
-export {
-  PreinstantiatedSingletonDefinition
-} from "./ioc/objectdefinition/PreinstantiatedSingletonDefinition";
-export {
-  BaseSingletonDefinition
-} from "./ioc/objectdefinition/BaseSingletonDefinition";
-export { InceptumApp } from "./app/InceptumApp";
-export * from "./log/LogManager";
-export { ExtendedError } from "./util/ErrorUtil";
-export { DBClient } from "./db/DBClient";
-export { DBTransaction } from "./db/DBTransaction";
+export { Context } from './ioc/Context';
+export { PreinstantiatedSingletonDefinition } from './ioc/objectdefinition/PreinstantiatedSingletonDefinition';
+export { BaseSingletonDefinition } from './ioc/objectdefinition/BaseSingletonDefinition';
+export { InceptumApp } from './app/InceptumApp';
+export * from './log/LogManager';
+export { ExtendedError } from './util/ErrorUtil';
+export { DBClient } from './db/DBClient';
+export { DBTransaction } from './db/DBTransaction';
 export { SqsHandler } from './sqs/SqsWorker';
-export { SwaggerPlugin } from "./swagger/SwaggerPlugin";
-export { default as WebPlugin } from "./web/WebPlugin";
-export { Plugin, PluginContext } from "./app/BaseApp";
+export { SwaggerPlugin } from './swagger/SwaggerPlugin';
+export { default as WebPlugin } from './web/WebPlugin';
+export { Plugin, PluginContext } from './app/BaseApp';
