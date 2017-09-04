429acc49ad676995e8d64a700a291fbf88546f58
Siyang Bi <siyang.bi@gmail.com>
2017-09-04 13:51:43 +1000

    *Export sqs worker handle class

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export * from './log/LogManager';
 export { ExtendedError } from './util/ErrorUtil';
 export { DBClient } from './db/DBClient';
 export { DBTransaction } from './db/DBTransaction';
+export { SqsHandler } from './sqs/SqsWorker';
 export { SwaggerPlugin } from './swagger/SwaggerPlugin';
 export { default as WebPlugin } from './web/WebPlugin';
 import { Plugin, PluginContext } from './app/BaseApp';
