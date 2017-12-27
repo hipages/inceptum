fdc8fbd658441d0119636cddda784f994706b4d8
Deepak Verma <deepakverma@hipagesgroup.com.au>
2017-12-27 15:46:59 +1100

    update: need more test cases here

--- itest/elasticsearch/ElasticsearchClient.ts
+++ itest/elasticsearch/ElasticsearchClient.ts
@@ -1,10 +1,9 @@
-import { must } from 'must';
-import { suite, test } from 'mocha-typescript';
 import * as elasticsearch from 'elasticsearch';
-import { ElasticsearchClient, ElasticsearchClientConfigObject } from '../../src/elasticsearch/ElasticsearchClient';
+import { must } from 'must';
+import { ElasticsearchClient } from '../../src/elasticsearch/ElasticsearchClient';
 
-const elasticConfig: ElasticsearchClientConfigObject = {
-  host: [{
+const elasticConfig = {
+  hosts: [{
     host: 'localhost',
     port: '9200',
     protocol: 'http',
@@ -12,21 +11,13 @@ const elasticConfig: ElasticsearchClientConfigObject = {
   apiVersion: '5.5',
 };
 
-describe.only('elasticsearchClient', () => {
-  let myClientConnection: ElasticsearchClient;
-  let myClient: elasticsearch.Client;
-  beforeEach(() => {
-    myClientConnection = new ElasticsearchClient('TestElasticSearchClient', elasticConfig);
-    myClientConnection.initialise();
-    myClient = myClientConnection.getClient();
-    },
-  );
+const myClient = new ElasticsearchClient('TestElasticSearchClient', elasticConfig);
+myClient.initialise();
+
+describe('elasticsearchClient', () => {
   describe('Connect', () => {
     it('Ping', () => {
-      return myClient.ping({
-        requestTimeout: 1000,
-      })
-      .then((result) => {
+      return myClient.ping().then((result) => {
         result.must.be.true();
       });
     });
@@ -35,16 +26,17 @@ describe.only('elasticsearchClient', () => {
     it('Index and Delete', () => {
       const data = {
         body: [
-          { index:  { _index: 'testindex', _type: 'testtype', _id: 1 } },
+          { index: { _index: 'testindex', _type: 'testtype', _id: 1 } },
           { title: 'foo1' },
 
-          { index:  { _index: 'testindex', _type: 'testtype', _id: 2 } },
+          { index: { _index: 'testindex', _type: 'testtype', _id: 2 } },
           { title: 'foo2' },
 
-          { delete:  { _index: 'testindex', _type: 'testtype', _id: 1 } },
-          { delete:  { _index: 'testindex', _type: 'testtype', _id: 2 } },
+          { delete: { _index: 'testindex', _type: 'testtype', _id: 1 } },
+          { delete: { _index: 'testindex', _type: 'testtype', _id: 2 } },
         ],
       };
+
       return myClient.bulk(data).then(
         (result) => {
           result.errors.must.be.false();
