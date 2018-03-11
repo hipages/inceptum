2b609b484d7d84205cc57d74ce224140814feb94
Paolo Ragone <paoloragone@hipagesgroup.com.au>
2018-03-11 11:45:55 +1100

    Adjusted bucket sizes to durations (as it is a histogram), instead of percentiles which would be right for a Summary

--- src/web/WebPlugin.ts
+++ src/web/WebPlugin.ts
@@ -58,7 +58,7 @@ export default class WebPlugin implements Plugin {
 
     express.use(promBundle({
       includeMethod: true,
-      buckets: [0.5, 0.75, 0.9, 0.99],
+      buckets: [0.003, 0.03, 0.1, 0.3, 0.5, 1.5, 10],
       autoregister: false,
     }));
 
