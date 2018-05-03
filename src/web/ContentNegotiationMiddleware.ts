485dc947a1d07cfbe74ab06cf95c197da68e8ca2
Wei Zhang <weizhang185@gmail.com>
2018-05-03 16:37:32 +1000

    Add XML Support middleware (#103)
    * Add XML Support middleware
    
    * remove xmlbuilder import
    
    * add comment
    
    * Added test and fixed small requests
    
    * Update Rabbitmq itest

---
+++ src/web/ContentNegotiationMiddleware.ts
@@ -0,0 +1,33 @@
+import * as xmlbuilder from 'xmlbuilder';
+
+/**
+ * To use this middleware with swagger, set xml root tag name in relevant definition in swagger.yml
+ */
+export class ContentNegotiationMiddleware {
+  private xmlRoot: string;
+
+  constructor(xmlRoot: string) {
+    this.xmlRoot = xmlRoot;
+  }
+
+  getMiddleware() {
+    const xmlRoot = this.xmlRoot;
+    if (!xmlRoot) {
+      return;
+    }
+    return (req, res, next) => {
+      const xmlType = 'application/xml';
+      if (req.is(xmlType) || xmlType === req.get('accept')) {
+        const originalSend = res.send;
+        res.send = function(...data) {
+          const obj = {};
+          obj[xmlRoot] = data[0];
+          data[0] = xmlbuilder.create(obj).end();
+          originalSend.apply(res, data);
+        };
+        res.header('content-type', xmlType);
+      }
+      next();
+    };
+  }
+}
