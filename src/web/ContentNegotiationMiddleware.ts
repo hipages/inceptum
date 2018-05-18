2d6b8258481461b4819be40ff22ea03d466965f3
Wei Zhang <weizhang@hipagesgroup.com.au>
2018-05-18 12:09:22 +1000

    remove content type

--- src/web/ContentNegotiationMiddleware.ts
+++ src/web/ContentNegotiationMiddleware.ts
@@ -17,7 +17,7 @@ export class ContentNegotiationMiddleware {
     }
     return (req, res, next) => {
       const xmlType = 'application/xml';
-      if (xmlType === req.get('Content-Type') || xmlType === req.get('accept')) {
+      if (xmlType === req.get('accept')) {
         const originalSend = res.send;
         res.send = function(...data) {
           const obj = {};
