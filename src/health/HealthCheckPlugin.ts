8e388d83207b8c24bb4af366e1095c13a5295d61
Wei Zhang <weizhang@hipagesgroup.com.au>
2018-06-12 08:40:54 +1000

    Health check always returns 200

--- src/health/HealthCheckPlugin.ts
+++ src/health/HealthCheckPlugin.ts
@@ -4,6 +4,7 @@ import BaseApp, { Plugin, PluginContext } from '../app/BaseApp';
 import { AutowireGroup, Lazy, StartMethod, StopMethod } from '../ioc/Decorators';
 import AdminPortPlugin from '../web/AdminPortPlugin';
 import { LogManager } from '../log/LogManager';
+import { NewrelicUtil } from '../newrelic/NewrelicUtil';
 import { HealthCheck, HealthCheckGroup, HealthCheckStatus, HEALTH_CHECK_GROUP } from './HealthCheck';
 import { ContextReadyHealthCheck } from './ContextReadyHealthCheck';
 
@@ -49,11 +50,10 @@ export default class HealthCheckPlugin implements Plugin {
       express.get('/health', async (req, res) => {
         const healthCheckManager: HealthCheckManager = await context.getObjectByName('HealthCheckManager');
         const healthCheckResult = healthCheckManager.getLastResult();
-        if (healthCheckResult.status === HealthCheckStatus.OK || healthCheckResult.status === HealthCheckStatus.NOT_READY) {
-          res.send(healthCheckResult);
-        } else {
-          res.status(503).send(healthCheckResult);
+        if (healthCheckResult.status !== HealthCheckStatus.OK && healthCheckResult.status !== HealthCheckStatus.NOT_READY) {
+          NewrelicUtil.noticeError(new Error(healthCheckResult.message), healthCheckResult);
         }
+        res.send(healthCheckResult);
       });
     }
   }
